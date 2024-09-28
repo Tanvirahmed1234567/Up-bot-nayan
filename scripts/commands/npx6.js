8487a7012e6c47136d463084664818b9a1d042c4
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-09-28 02:38:30 +0000

    Update npx6.js

--- scripts/commands/npx6.js
+++ scripts/commands/npx6.js
@@ -17,7 +17,7 @@ handleEvent: async function({ api, event, client, __GLOBAL }) {
   const content = event.body ? event.body : '';
   const body = content.toLowerCase();
   const axios = require('axios')
-  const NAYAN = ["https://i.imgur.com/32DFsr8.mp4","https://i.imgur.com/ok6SloW.mp4","https://i.imgur.com/MCAwODI.mp4","https://i.imgur.com/OK7DdJL.mp4","https://i.imgur.com/0RKxzyC.mp4","https://i.imgur.com/Fl40TtV.mp4","https://i.imgur.com/r0E3tOP.mp4",'https://i.imgur.com/2oUXs44.mp4','https://i.imgur.com/gjaulFD.mp4','https://i.imgur.com/D0127fR.mp4','https://i.imgur.com/rdV22l6.mp4','https://i.imgur.com/gg6Vh6x.mp4','https://i.imgur.com/naq7XwF.mp4','https://i.imgur.com/DdErHEM.mp4','https://i.imgur.com/Gee37C0.mp4']
+  const NAYAN = ["https://i.imgur.com/8xcGK2A.mp4","https://i.imgur.com/ZBBp7do.mp4","https://i.imgur.com/96VjXF8.mp4","https://i.imgur.com/Hz2czIS.mp4","https://i.imgur.com/h0p8Xdx.mp4","https://i.imgur.com/fztqIO8.mp4","https://i.imgur.com/1Zz9aVo.mp4","https://i.imgur.com/32DFsr8.mp4","https://i.imgur.com/ok6SloW.mp4","https://i.imgur.com/MCAwODI.mp4","https://i.imgur.com/OK7DdJL.mp4","https://i.imgur.com/0RKxzyC.mp4","https://i.imgur.com/Fl40TtV.mp4","https://i.imgur.com/r0E3tOP.mp4",'https://i.imgur.com/2oUXs44.mp4','https://i.imgur.com/gjaulFD.mp4','https://i.imgur.com/D0127fR.mp4','https://i.imgur.com/rdV22l6.mp4','https://i.imgur.com/gg6Vh6x.mp4','https://i.imgur.com/naq7XwF.mp4','https://i.imgur.com/DdErHEM.mp4','https://i.imgur.com/Gee37C0.mp4']
     var rndm = NAYAN[Math.floor(Math.random() * NAYAN.length)];
 const media = (
     await axios.get(

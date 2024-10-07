1b294c03bce486396e2e4f4ebfe40ac66f47fa85
Tanvirahmed1234567 <dipto123132@gmail.com>
2024-10-07 19:53:13 +0600

    Update addmin3.js

--- scripts/commands/addmin3.js
+++ scripts/commands/addmin3.js
@@ -42,6 +42,6 @@ var callback = () => api.sendMessage({body:`
 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  :- t.me/𝗽𝗶𝗶𝘅𝘅𝗶𝘁𝗮𝗵
 𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :-https://www.facebook.com/pixxi0`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/61566560938302/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/61566493245098/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
